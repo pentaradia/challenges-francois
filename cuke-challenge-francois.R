5be31942f0717a06d0a4dd709a684f178d278a0e
pentaradia <fmichon@gmail.com>
2015-02-20 15:48:55 -0500

    Update cuke-challenge-francois.R

--- cuke-challenge-francois.R
+++ cuke-challenge-francois.R
@@ -3,6 +3,8 @@ download.file("http://r-bio.github.io/data/holothuriidae-specimens.csv",
 download.file("http://r-bio.github.io/data/holothuriidae-nomina-valid.csv",
               "data/holothuriidae-nomina-valid.csv")
 
+read.csv(file="data/holothuriidae-specimens.csv")
+
 ## another test
 ## silly test
 
